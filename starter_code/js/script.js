eb3895066d79b30360e43ebb7bf91b7cdb127892
julissa2017 <judiaz@cristoreyny.org>
2016-02-04 13:49:17 +0000

    My Flickr Search Project

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,6 +1,21 @@
 // YOUR CODE GOES HERE
-var clientID = "REPLACE ME WITH CLIENT ID";
+var clientID = "3565fe59fc166cb7f5a0ab9b7c982cc8";
 
 $(document).ready(function() {
-    // your code here
+    $("#search-btn").click(function() {
+        var userInput = $("#search-query").val();
+
+        searchFlickr(clientID, userInput, 10, function(photos) {
+            console.log(photos.photo[0].title);
+            for (var i = 0; i < photos.photo.length; i++) {
+                photos.photo[i].url_s;
+                $("#search-results").append("<img src = " + photos.photo[i].url_s +">");
+                
+            }
+
+        });
+    });
+    $("#reset-btn").click(function(){
+        $("#search-results").empty();
+    })
 });
